36e3f57e9c8348df4df0d0e2f13239b071dda007
Yann Zaoui <contact@yannzaoui.com>
2026-01-11 15:24:26 -0500

    Add guard to DexTable.insertDiscovered

--- backend/app/src/main/kotlin/db/usecase/MarketUCImpl.kt
+++ backend/app/src/main/kotlin/db/usecase/MarketUCImpl.kt
@@ -119,7 +119,9 @@ class MarketUCImpl(
         /* Insert items into user's inventory */
         inventoryTable.insertItems(userId, userItemsToInsert)
         /* Insert items into user's dex */
-        dexTable.insertDiscovered(userId, dexItemsToInsert)
+        if (dexItemsToInsert.isNotEmpty()) {
+            dexTable.insertDiscovered(userId, dexItemsToInsert)
+        }
         /* Update quantity of user's items */
         userItemsToUpdateQuantity.forEach { materializedItemTable.updateQuantity(it.key, it.value) }
         /* Remove game's items */
