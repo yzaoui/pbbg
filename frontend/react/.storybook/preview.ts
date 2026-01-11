dd98e90b9fe9e89cb8765b6caea209d1577dfbb6
Yann Zaoui <contact@yannzaoui.com>
2026-01-11 17:50:41 -0500

    Add Storybook

---
+++ frontend/react/.storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from "@storybook/react";
+import "normalize.css";
+import "../src/common.scss";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
